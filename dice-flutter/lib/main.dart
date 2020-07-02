ca0b1c7d0ee91da791ac691d139974d6038db342
Chirag Jain <me.chiragjain@gmail.com>
2020-07-02 17:01:04 +0530

    Dice App Designed

---
+++ dice-flutter/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dice'),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image(
+                image: AssetImage('images/dice1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image(
+                image: AssetImage('images/dice1.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
