ed8b3c60a28d7bedc5986d2785ba11ae8cbf5848
Chirag Jain <me.chiragjain@gmail.com>
2020-07-06 23:38:59 +0530

    Quiz App - Questions Display

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'questions.dart';
 
 void main() => runApp(Quizzler());
 
@@ -36,6 +37,19 @@ class _QuizPageState extends State<QuizPage> {
     ),
   ];
 
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  List<bool> answers = [false, true, true];
+
+  Questions q1 = Questions(
+      ques: 'You can lead a cow down stairs but not up stairs.', ans: false);
+
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -48,7 +62,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,7 +87,16 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+
+                bool correctAnswer = answers[questionNumber];
+
+                if (correctAnswer == true) {
+                  print('Correct');
+                } else {
+                  print('Wrong');
+                }
                 setState(() {
+                  questionNumber++;
                   scoreKeeper.add(
                     Icon(
                       Icons.check,
@@ -99,6 +122,24 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+
+                bool correctAnswer = answers[questionNumber];
+
+                if (correctAnswer == false) {
+                  print('Correct');
+                } else {
+                  print('Wrong');
+                }
+
+                setState(() {
+                  questionNumber++;
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.close,
+                      color: Colors.red,
+                    ),
+                  );
+                });
               },
             ),
           ),
---
+++ quizzler-flutter/lib/questions.dart
@@ -0,0 +1,9 @@
+class Questions {
+  String questionText;
+  bool questionAnswer;
+
+  Questions({String ques, bool ans}) {
+    questionText = ques;
+    questionAnswer = ans;
+  }
+}
